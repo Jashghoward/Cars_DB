7017555f0b1beda159edca16e3ee83ee2bc7d288
Josh Howard <jashghoward@icloud.com>
2023-09-02 22:40:25 +0200

    engine.ex update

---
+++ lib/cars_db/engine.ex
@@ -0,0 +1,20 @@
+defmodule CarsDb.Engine do
+  import Ecto.Changeset
+  use Ecto.Schema
+  alias CarsDb.{Model}
+
+  schema "engines" do
+    field(:type, :string)
+    field(:capaciry, :string)
+    timestamps()
+
+    belongs_to(:model, Model)
+  end
+
+  def changeset(engine, params \\ %{}) do
+    engine
+    |> cast(params, [:type, :capacity, :model_id])
+    |> validate_required([:type, :capacity, :model_id])
+  end
+
+end
