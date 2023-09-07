f5d83e2e00730c51c524642bd2df9ac1de3fd740
Josh Howard <jashghoward@icloud.com>
2023-09-07 22:05:06 +0200

    make.ex update

---
+++ priv/queries/create_engine.exs
@@ -0,0 +1,7 @@
+alias MyTest.{Repo, Movie}
+
+# Insert the new movie into the database
+new_movie = %Movie{title: "New Movie", category: "Action", shop_id: 1} # Replace 1 with the actual shop ID
+Repo.insert!(new_movie)
+
+IO.puts("New movie added successfully.")
