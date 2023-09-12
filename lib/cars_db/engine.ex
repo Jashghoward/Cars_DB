022015e40a5343dad95477271830e556e243df5c
Josh Howard <jashghoward@icloud.com>
2023-09-12 23:50:19 +0200

    model.ex update

--- lib/cars_db/engine.ex
+++ lib/cars_db/engine.ex
@@ -17,6 +17,8 @@ defmodule CarsDb.Engine do
   def changeset(engine, params \\ %{}) do
     engine
 
+
+
     |> cast(params, [:type, :capacity, :model_id])
     |> validate_required([:type, :capacity, :model_id])
   end
