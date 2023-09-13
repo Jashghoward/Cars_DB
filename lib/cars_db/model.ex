6ea485853d75ea77e9e5e7ae5a252a44d834ac02
Josh Howard <jashghoward@icloud.com>
2023-09-13 21:39:11 +0200

    model.ex update

--- lib/cars_db/model.ex
+++ lib/cars_db/model.ex
@@ -13,11 +13,7 @@ defmodule CarsDb.Model do
 
   def changeset(model, params \\ %{}) do
     model
-
-
     |> cast(params, [:title, :category, :make_id])
-
-
     |> validate_required([:title, :category, :make_id])
   end
 end
