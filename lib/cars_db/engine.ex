77ab9a38ff5cd9f6cc1060e1f819b4fef72e4a8e
Josh Howard <jashghoward@icloud.com>
2023-10-26 23:30:57 +0200

    engine.ex update

--- lib/cars_db/engine.ex
+++ lib/cars_db/engine.ex
@@ -37,10 +37,6 @@ defmodule CarsDb.EngineContext do
   def list_engines do
     all_engines = Repo.all(Engine)
     Enum.each(all_engines, fn engine ->
-
-
-
-
       IO.puts("#{engine.name}")
     end)
 
