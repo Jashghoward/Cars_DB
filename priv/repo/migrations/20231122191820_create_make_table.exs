71010dc1551800ac8582bbc325c1730d0732562f
Josh Howard <jashghoward@icloud.com>
2023-11-28 20:43:36 +0200

    engine.ex update

---
+++ priv/repo/migrations/20231122191820_create_make_table.exs
@@ -0,0 +1,12 @@
+defmodule CarsDb.Repo.Migrations.CreateMakeTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:make) do
+      add :name, :string
+      timestamps()
+
+
+    end
+  end
+end
